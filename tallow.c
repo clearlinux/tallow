67c388a12e4bae39eee7efb66a7d15acc8f44900
Auke Kok <auke@foo-projects.org>
2012-10-29 14:55:09 -0700

    License header in the C file.

--- tallow.c
+++ tallow.c
@@ -1,3 +1,15 @@
+/*
+ * tallow.c - IP block sshd login abuse
+ *
+ * (C) Copyright 2012 Intel Corporation
+ * Authors:
+ *     Auke Kok <auke@linux.intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 3
+ * of the License.
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
