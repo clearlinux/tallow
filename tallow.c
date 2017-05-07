992927798db3df5bcec14ab0f96e94d73ed9627a
Auke Kok <sofar@foo-projects.org>
2017-05-06 22:30:50 -0700

    Convert to ipset.
    Create `tallow` and `tallow6` ipsets, hook up to iptables
    and create a single rule in the INPUT chain of the filter
    table.
    
    The ipsets created have `expire` timeouts set by default
    which removes the need to do pruning, so we can erase entries
    immediately from our LL when blocking.

--- tallow.c
+++ tallow.c
@@ -35,7 +35,7 @@ static struct tallow_struct *whitelist;
 
 #define FILTER_STRING "SYSLOG_IDENTIFIER=sshd"
 
-static char iptables_path[PATH_MAX];
+static char ipt_path[PATH_MAX];
 static char chain[PATH_MAX];
 static int threshold = 3;
 static int expires = 3600;
@@ -67,7 +67,7 @@ static void ext_ignore(char *fmt, ...)
 	vsnprintf(cmd, sizeof(cmd), fmt, args);
 	va_end(args);
 
-	(void) system(cmd);
+	__attribute__((unused)) int ret = system(cmd);
 }
 
 static void block(struct tallow_struct *s)
@@ -77,27 +77,24 @@ static void block(struct tallow_struct *s)
 
 	if (strchr(s->ip, ':')) {
 		if (has_ipv6)
-			(void) ext("%s/ip6tables -t filter -A %s -s %s -j DROP", iptables_path, chain, s->ip);
+			(void) ext("%s/ipset -A tallow6 %s", ipt_path, s->ip);
 	} else {
-		(void) ext("%s/iptables -t filter -A %s -s %s -j DROP", iptables_path, chain, s->ip);
+		(void) ext("%s/ipset -A tallow %s", ipt_path, s->ip);
 	}
 
 	fprintf(stderr, "Blocked %s\n", s->ip);
-}
 
-static void unblock(struct tallow_struct *s)
-{
-	if (s->count < threshold)
-		return;
-
-	if (strchr(s->ip, ':')) {
-		if (has_ipv6)
-			(void) ext("%s/ip6tables -t filter -D %s -s %s -j DROP", iptables_path, chain, s->ip);
+	/* remove entry from the list */
+	if (head == s) {
+		head = s->next;
+		free(s->ip);
+		free(s);
 	} else {
-		(void) ext("%s/iptables -t filter -D %s -s %s -j DROP", iptables_path, chain, s->ip);
+		struct tallow_struct *p = s;
+		s = s->next;
+		free(p->ip);
+		free(p);
 	}
-
-	fprintf(stderr, "Unblocked %s\n", s->ip);
 }
 
 static void whitelist_add(char *ip)
@@ -214,47 +211,14 @@ static void sig(int s)
 	}
 }
 
-static void prune(void)
-{
-	struct tallow_struct *s = head;
-	struct tallow_struct *p;
-	struct timeval tv;
-
-	(void) gettimeofday(&tv, NULL);
-	p = NULL;
-
-	while (s) {
-		if ((tv.tv_sec - s->time.tv_sec) > expires) {
-			if (p) {
-				unblock(s);
-				p->next = s->next;
-				free(s->ip);
-				free(s);
-				s = p->next;
-				continue;
-			} else {
-				unblock(s);
-				head = s->next;
-				free(s->ip);
-				free(s);
-				s = head;
-				p = NULL;
-				continue;
-			}
-		}
-		p = s;
-		s = s->next;
-	}
-}
-
-int main(int argc, char *argv[])
+int main(void)
 {
 	int r;
 	FILE *f;
 	struct sigaction s;
 	int timeout = 60;
 
-	strcpy(iptables_path, "/usr/sbin");
+	strcpy(ipt_path, "/usr/sbin");
 	strcpy(chain, "TALLOW");
 
 	memset(&s, 0, sizeof(struct sigaction));
@@ -291,8 +255,8 @@ int main(int argc, char *argv[])
 
 			// todo: filter leading/trailing whitespace
 
-			if (!strcmp(key, "iptables_path"))
-				strncpy(iptables_path, val, PATH_MAX - 1);
+			if (!strcmp(key, "ipt_path"))
+				strncpy(ipt_path, val, PATH_MAX - 1);
 			if (!strcmp(key, "chain"))
 				strncpy(chain, val, PATH_MAX - 1);
 			if (!strcmp(key, "threshold"))
@@ -319,17 +283,22 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	/* init ipsets */
+	ext_ignore("%s/ipset create tallow hash:ip timeout %d", expires);
+	if (has_ipv6)
+		ext_ignore("%s/ipset create tallow6 hash:ip timeout %d", expires);
+
 	/* init ip(6)tables chains */
-	ext_ignore("%s/iptables -t filter -N %s > /dev/null 2>&1", iptables_path, chain);
-	if (ext("%s/iptables -t filter -F %s", iptables_path, chain)) {
-		fprintf(stderr, "Unable to create/flush iptables chain \"%s\".\n", chain);
+	ext_ignore("%s/iptables -t filter -D INPUT -m set --match-set tallow src -j DROP", ipt_path);
+	if (ext("%s/iptables -t filter -D INPUT -m set --match-set tallow src -j DROP", ipt_path)) {
+		fprintf(stderr, "Unable to create iptables rule.\n");
 		exit(1);
 	}
 
 	if (has_ipv6) {
-		ext_ignore("%s/ip6tables -t filter -N %s > /dev/null 2>&1", iptables_path, chain);
-		if (ext("%s/ip6tables -t filter -F %s", iptables_path, chain)) {
-			fprintf(stderr, "Unable to create/flush ip6tables chain \"%s\".\n", chain);
+		ext_ignore("%s/ip6tables -t filter -D INPUT -m set --match-set tallow src -j DROP", ipt_path);
+		if (ext("%s/ip6tables -t filter -D INPUT -m set --match-set tallow src -j DROP", ipt_path)) {
+			fprintf(stderr, "Unable to create ipt6ables rule.\n");
 			exit(1);
 		}
 	}
@@ -384,8 +353,6 @@ int main(int argc, char *argv[])
 
 			free(m);
 		}
-
-		prune();
 	}
 
 	sd_journal_close(j);
